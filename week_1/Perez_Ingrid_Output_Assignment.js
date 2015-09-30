f2de38421625ead5207b6ef959e07716923d5dce
ingridkathyria <iperez@fullfail.edu>
2015-09-30 19:01:38 -0400

    added variable

--- week_1/Perez_Ingrid_Output_Assignment.js
+++ week_1/Perez_Ingrid_Output_Assignment.js
@@ -8,14 +8,15 @@ var myDaughters; //declares variable
 var kairiAge; //declares variable
 var ariaAge; //declares variable
 var hasDaughters; //declares variable
-
+var numDaughters; //declares variable
 // variables
 myDaughters =("Kairi and Aria"); //defines variable
 kairiAge =("2 years old"); //defines variable
 ariaAge =("8 months old"); //defines variable
 hasDaughters =("true"); //defines variable
+numDaughters =2;
 
-
-// outputs  I have two beautiful daughter's. Kairi and Aria, kairi is 2 years old and aria is going to be 8 months old soon.
+// outputs  I have 2 beautiful daughter's. Kairi and Aria, kairi is 2 years old and aria is going to be 8 months old soon.
 // I was a manager at Olive garden and when I had Kairi I decided that I don't wanna line in the restaurant all day and night.
 // I want a job that I can enjoy and make enough money to give my daughter's more than I had.
+
