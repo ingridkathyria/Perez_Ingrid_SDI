d41003fae9f851e55560aa55b85bb92ea24265e1
ingridkathyria <iperez@fullfail.edu>
2015-09-30 18:57:10 -0400

    added variables

---
+++ week_1/Perez_Ingrid_Output_Assignment.js
@@ -0,0 +1,21 @@
+/**
+ Ingrid K. Perez
+ SDI 1015
+ Output Assignment
+ */
+
+var myDaughters; //declares variable
+var kairiAge; //declares variable
+var ariaAge; //declares variable
+var hasDaughters; //declares variable
+
+// variables
+myDaughters =("Kairi and Aria"); //defines variable
+kairiAge =("2 years old"); //defines variable
+ariaAge =("8 months old"); //defines variable
+hasDaughters =("true"); //defines variable
+
+
+// outputs  I have two beautiful daughter's. Kairi and Aria, kairi is 2 years old and aria is going to be 8 months old soon.
+// I was a manager at Olive garden and when I had Kairi I decided that I don't wanna line in the restaurant all day and night.
+// I want a job that I can enjoy and make enough money to give my daughter's more than I had.
