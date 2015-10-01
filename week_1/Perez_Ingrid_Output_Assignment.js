0cf0baf6c9c88590ba01ab34ad5ea9189fe1a40f
ingridkathyria <iperez@fullfail.edu>
2015-09-30 20:26:46 -0400

    added var to output

--- week_1/Perez_Ingrid_Output_Assignment.js
+++ week_1/Perez_Ingrid_Output_Assignment.js
@@ -10,14 +10,14 @@ var ariaAge; //declares variable
 var hasDaughters; //declares variable
 var numDaughters; //declares variable
 // variables
-myDaughters =("Kairi and Aria"); //defines variable
-kairiAge =("2 years old"); //defines variable
-ariaAge =("8 months old"); //defines variable
-hasDaughters =("true"); //defines variable
+myDaughters ="Kairi and Aria"; //defines variable
+kairiAge ="2 years old"; //defines variable
+ariaAge ="8 months old"; //defines variable
+hasDaughters =true ; //defines variable
 numDaughters =2;
 
 // outputs  I have 2 beautiful daughters. Kairi and Aria, Kairi is 2 years old and aria is going to be 8 months old soon.
-output =" I have " + numDaughters + " beautiful daughters. " + myDaughters + ", Kairi is " + kairiAge + " and Aria is going to be " + ariaAge + " soon.";
+output =" It is " + hasDaughters + " that I have " + numDaughters + " beautiful daughters. " + myDaughters + ", Kairi is " + kairiAge + " and Aria is going to be " + ariaAge + " soon.";
 
 
 console.log (output);
