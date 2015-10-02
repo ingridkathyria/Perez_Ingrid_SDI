f07878f3ee9935f8da65d4d689a0a9640289b6d4
ingridkathyria <iperez@fullfail.edu>
2015-10-02 15:40:14 -0400

    assigned var

--- week 2/Expressions_worksheet/expressionWorksheet.js
+++ week 2/Expressions_worksheet/expressionWorksheet.js
@@ -4,7 +4,7 @@
 
 
 //Calculate how old is Sparky the pitbull in dog years based on his actual age.
-
+/*
 var result;
 var humanYears;
 var dogYears;
@@ -18,3 +18,19 @@ sparkysAge = humanYears * dogYears;
 //result = "Sparky is " + humanYears + " human year old which is " + dogYears + " in dog years. So in three years, Sparky will be " + sparkysAge * 3 + "."";
 result = " Sparky is "+ humanYears + " human year old which is " + dogYears + " in dog years. So in three years, Sparky will be " + sparkysAge * 3 + ".";
 console.log(result);
+*/
+
+// Create an expression that calculates how much pizza each partygoer will get at the party.
+
+var pizzaSlices;
+var peopleAtPart;
+var numPizzas;
+var slicesPerPerson;
+
+pizzaSlices = 8;
+peopleAtParty = 10;
+numPizzas = 4;
+slicesPerPerson = (pizzaSlices * numPizzas)/peopleAtParty;
+
+
+
