dbb817670cbfb4591d4284ac01fbb934696a4454
ingridkathyria <iperez@fullfail.edu>
2015-10-02 21:09:00 -0400

    declare var

--- week_1/values and variables/valuesVariables.js
+++ week_1/values and variables/valuesVariables.js
@@ -1,11 +1,13 @@
-
-
-var yearBorn = 1980;
 /*
+//declaring var
+var yearBorn = 1980;
+
 console.log("Hello World!");
 console.log(yearBorn);
 
 alert("Hello World!")
  */
-
+//Defining variable
 var age = 35;
+
+var yearOFBirth = 1980;
