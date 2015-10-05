8d6e6736f31d6c015282a17e89dfb90c18a7a5e7
ingridkathyria <iperez@fullfail.edu>
2015-10-04 20:53:02 -0400

    reviewed codes

--- week 2/Expressions_worksheet/expressionWorksheet.js
+++ week 2/Expressions_worksheet/expressionWorksheet.js
@@ -28,7 +28,7 @@ var numPizzas;//declared variable
 var slicesPerPerson;//declared variable
 var result;//declared variable
 
-pizzaSlices = 8;//assigned variable
+pizzaSlices = 8;// assigned variables
 peopleAtParty = 10;// assigned variables
 numPizzas = 4;//assigned variables
 slicesPerPerson = (pizzaSlices * numPizzas)/peopleAtParty;
@@ -50,41 +50,42 @@ console.log("Sparky got " + sparkySlices + " slices of pizza.");
 /*
 // Create an expression that will use the items in the array to calculate the average amount spent on groceries.
 
-var weeklyTotals;
-var totalSpent;
-var averageWeekly;
+var weeklyTotals;//declared variable
+var totalSpent;//declared variable
+var averageWeekly;//declared variable
+var result;
 
-weeklyTotals = [25, 45, 40, 38, 50];
-totalSpent = 25 + 45 + 40 + 38 + 50;
-averageWeekly = totalSpent / 5;
-
+weeklyTotals = [25, 45, 40, 38, 50];//assigned variable
+totalSpent = 25 + 45 + 40 + 38 + 50;//assigned variable
+averageWeekly = totalSpent / 5;//assigned variable
+result = "You have spent a total of $" + totalSpent + " on groceries over 5 weeks. That is an average of $" + averageWeekly + " per week."
 //“You have spent a total of $X on groceries over 5 weeks. That is an average of $X per week”
-console.log("You have spent a total of $" + totalSpent + " on groceries over 5 weeks. That is an average of $" + averageWeekly + " per week.")
+console.log(result)
 */
 
 
 
 // Create an expression that will calculate the discounted price with and without sales tax.
 
-var originalPrice;
-var discountPercent;
-var item;
-var salesTax;
-var itemDiscount;
-var taxedItem;
-var priceItemTax;
-var itemAfterDiscount;
-var result;
+var originalPrice;//declared variable
+var discountPercent;//declared variable
+var item;//declared variable
+var salesTax;//declared variable
+var itemDiscount;//declared variable
+var taxedItem;//declared variable
+var priceItemTax;//declared variable
+var itemAfterDiscount;//declared variable
+var result;//declared variable
 
-originalPrice = 5;
-discountPercent = .10;
-item = "shoes";
-salesTax = 0.06;
-itemDiscount =  originalPrice * discountPercent;
-itemAfterDiscount = originalPrice - itemDiscount;
-taxedItem = salesTax * itemAfterDiscount;
-priceItemTax = originalPrice + salesTax;
+originalPrice = 5;// assigned variables
+discountPercent = 0.10;// assigned variables
+item = "shoes";// assigned variables
+salesTax = 0.06;// assigned variables
+itemDiscount =  originalPrice * discountPercent;// assigned variables
+itemAfterDiscount = originalPrice - itemDiscount;// assigned variables
+taxedItem = salesTax * itemAfterDiscount;// assigned variables
+priceItemTax = originalPrice + salesTax;// assigned variables
 
-result = "Your " + item + " was originally $" + originalPrice + " but after a " + discountPercent + "% discount, it is now $" + itemAfterDiscount +
+result = "Your " + item + " was originally $" + originalPrice + " but after a " + discountPercent + "% discount, it is now $" + itemAfterDiscount + " without tax, and $" + priceItemTax + " with tax.";
 //“Your shoes was originally $5, but after a 10% discount, it is now $X without tax, and $X with tax.”
-console.log(result);
+console.log(result);// assigned console.log
