d715f9f8643a6a68dd20648dcbb9aa02bb4364b8
ingridkathyria <iperez@fullfail.edu>
2015-10-06 19:17:18 -0400

    added strings
    assigned variables and add console logs

--- week 2/Perez_Ingrid_Epressions_Assignment/scripts.js
+++ week 2/Perez_Ingrid_Epressions_Assignment/scripts.js
@@ -1,16 +1,21 @@
 /*Ingrid Perez
-* October 4,2015.
-*Expression Assignment
+ * October 4,2015.
+ *Expression Assignment
 */
 
 
 //declared variables
-var myAge;
 var myHeight;
 var myWeight;
 var myGoal;
+var poundsTOLose;
 
-myAge = prompt("Enter your age:");
+//assigned variable
 myHeight = prompt("Enter your height:");
 myWeight = prompt("Enter your weight: ");
 myGoal = prompt("What is your weight goal?");
+poundsTOLose = myWeight - myGoal;
+
+
+console.log("You are " + myHeight + " tall and weigh " + myWeight + " pounds.");
+console.log("To reach your goal you of " + myGoal + " you need to lose " + (myWeight - myGoal) + " pounds." )
