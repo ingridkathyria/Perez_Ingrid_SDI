3cd3cfcb8136db42e7e938b8ae7524dc3e1a2dbc
ingridkathyria <iperez@fullfail.edu>
2015-10-06 18:28:58 -0400

    declared variables

--- week 2/Perez_Ingrid_Epressions_Assignment/scripts.js
+++ week 2/Perez_Ingrid_Epressions_Assignment/scripts.js
@@ -1,4 +1,16 @@
-//Ingrid Perez
-// October 4,2015.
-//Expression Assignment
+/*Ingrid Perez
+* October 4,2015.
+*Expression Assignment
+*/
 
+
+//declared variables
+var myAge;
+var myHeight;
+var myWeight;
+var myGoal;
+
+myAge = prompt("Enter your age:");
+myHeight = prompt("Enter your height:");
+myWeight = prompt("Enter your weight: ");
+myGoal = prompt("What is your weight goal?");
