9f0d454fdf609f0d41f48769e0a7b2a46d958c46
ingridkathyria <iperez@fullfail.edu>
2015-10-09 09:06:41 -0400

    created new folder

---
+++ week 3/Conditionals/script.js
@@ -0,0 +1,14 @@
+/*Ingrid Perez
+Conditionals
+*/
+
+// If statements = if(statement ex sunny){goToTheBeach();}else{goToTheMovies();
+//if(statement ex sunny){goToTheBeach()if(waterWarm){wearNewSuit();};
+
+var oldEnough =  true;
+
+//if the chil is old enough, print to the console "you can ride!
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!")
+}
