7e557c76d197c12b5d38ba328c4d87a08475d262
ingridkathyria <iperez@fullfail.edu>
2015-10-10 20:33:53 -0400

    added problem

---
+++ week 3/Conditionals_worksheet/script.js
@@ -0,0 +1,40 @@
+//Ingrid Perez
+//October 10,2015.
+//Conditionals Worksheet
+
+
+
+
+
+//A driver has to determine if they can make it across the desert with their current fuel.
+// They are about to pass the last gas station for the next 200 miles,
+// and they need to determine whether they should stop now for gas or not.
+
+var carEfficiency;
+var gaugeReading;
+var tankCapacity;
+var gallonsNeeded;
+
+//Gas efficiency of the car (in mpg)
+carEfficiency = 30 mpg;
+//Gauge reading of the gas tank (in %)
+gaugeReading = 7 gallons;
+//Car’s gas tank capacity (in gallons)
+tankCapacity = 12 gallons;
+//Gallons needed to cross the desert
+gallonsNeeded = 200 / 30
+
+if(gaugeReading > gallonsNeeded){
+    console.log("Yes, you can make it without stopping for gas!")
+}
+
+
+
+
+
+
+
+
+
+//“Yes, you can make it without stopping for gas!”
+// or “You only have X gallons of gas in your tank, better get gas now while you can!”
