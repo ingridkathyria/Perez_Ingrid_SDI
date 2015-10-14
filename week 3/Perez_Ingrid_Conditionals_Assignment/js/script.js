512f18a4e7b712f7fa8592c945d3ff74c59be1eb
ingridkathyria <iperez@fullfail.edu>
2015-10-14 13:23:03 -0400

    added variables
    declared and assigned variables

--- week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
+++ week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
@@ -9,11 +9,15 @@ var billAmount;
 var taxPercent;
 var tipAmount;
 var billTax;
+var totalBill;
 tipPercent = [ 10, 15, 20];
 
 billAmount = prompt("What is the bill amount?");
-tipPercent[0]= prompt("What percentage would you like to tip?"+[10] +" "+[15]+" or "+[20]);
+tipPercent[0]= prompt("What percentage would you like to tip?"+" "+[10] +", "+[15]+" or "+[20]);
 taxPercent = prompt("What is the tax percent in your area?");
 tipAmount = tipPercent * billAmount;
 billTax = billAmount * taxPercent;
+totalBill = (billAmount + tipAmount) + billTax;
+
+if()
 
