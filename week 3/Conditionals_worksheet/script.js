36f2c57aa6512247f81199ab6ab67a1e6e9eb93f
ingridkathyria <iperez@fullfail.edu>
2015-10-14 10:54:06 -0400

    created folder
    created folder and tested the js file

--- week 3/Conditionals_worksheet/script.js
+++ week 3/Conditionals_worksheet/script.js
@@ -61,7 +61,7 @@ if(usernameEntered == correctUsername && passwordEntered == correctPassword){
 //“User not found. Try again.” -if the username does not match
 //“Password does not match our records.”  -if the username matches but the password does not
 */
-
+/*
 //The local movie theater in town has a ticket price of $12.00 but if you are a senior (55 and older), under 10 you get
 // the discounted price of $7.00. In addition, if a customer is seeing a movie between 3pm and 5pm they can also get the discounted price.
 //Determine which of the two prices the customer is eligible for.
@@ -85,3 +85,4 @@ if(movieTime <= 5 && movieTime >= 3 || customerAge <= 10 || customerAge >= 55){
 }else{
     console.log("Ticket price is $"+ regularTicket + ".");
 }
+    */
