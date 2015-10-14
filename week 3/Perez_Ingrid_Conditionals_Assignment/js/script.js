cb289836d0f571693a05204da4205650d5d2ffc8
ingridkathyria <iperez@fullfail.edu>
2015-10-14 14:00:10 -0400

    adjusted variables
    removed array, edited variables in lines 15 thru 20

--- week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
+++ week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
@@ -10,14 +10,18 @@ var taxPercent;
 var tipAmount;
 var billTax;
 var totalBill;
-tipPercent = [ 10, 15, 20];
+var tipPercent;
 
-billAmount = prompt("What is the bill amount?");
-tipPercent[0]= prompt("What percentage would you like to tip?"+" "+[10] +", "+[15]+" or "+[20]);
-taxPercent = prompt("What is the tax percent in your area?");
+billAmount = Number(prompt("What is the bill amount?"));
+tipPercent= Number(prompt("What percentage would you like to tip?"));
+taxPercent = Number(prompt("What is the tax percent in your area?"));
 tipAmount = tipPercent * billAmount;
 billTax = billAmount * taxPercent;
 totalBill = (billAmount + tipAmount) + billTax;
 
-if()
+if(tipPercent < 0 ){
+    console.log("Thank you! Have a nice day!")
+}else{
+
+}
 
