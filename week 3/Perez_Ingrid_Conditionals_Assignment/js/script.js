ffc07b52ba680cd30573abc36adf229d58b7e238
ingridkathyria <iperez@fullfail.edu>
2015-10-14 13:17:59 -0400

    added comments

--- week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
+++ week 3/Perez_Ingrid_Conditionals_Assignment/js/script.js
@@ -1,1 +1,19 @@
-alert("This is in the js file");
+//Ingrid Perez
+//October 14,2015.
+//Conditionals Assignment
+
+
+//Calculate the tip for a server in a restaurant
+
+var billAmount;
+var taxPercent;
+var tipAmount;
+var billTax;
+tipPercent = [ 10, 15, 20];
+
+billAmount = prompt("What is the bill amount?");
+tipPercent[0]= prompt("What percentage would you like to tip?"+[10] +" "+[15]+" or "+[20]);
+taxPercent = prompt("What is the tax percent in your area?");
+tipAmount = tipPercent * billAmount;
+billTax = billAmount * taxPercent;
+
