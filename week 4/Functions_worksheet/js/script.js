cb5d8800d0fcf462af21ab37598772b3e877840a
ingridkathyria <iperez@fullfail.edu>
2015-10-19 15:22:09 -0400

    worksheet
    practiced 2 examples of functions

--- week 4/Functions_worksheet/js/script.js
+++ week 4/Functions_worksheet/js/script.js
@@ -2,9 +2,30 @@
 //October 19, 2015.
 //Functions worksheet
 
-
-function circumference(pi,diameter){
+/*
+//Calculate the circumference of a circle.
+var pi = 3.14;
+var diameter = 4;
+//function with calculation added in the return
+ function circumference(pi,diameter){
     return pi * diameter;
 }
 
 console.log("The circumference of a circle is " + circumference(3.14,4)+".");
+
+*/
+
+/*
+//It takes 8.666666667 bee stings per pound to kill an animal.
+// Calculate how many bee stings are needed to kill an animal in a function
+
+var beeStings = 8.666666667;
+var animalWeightLbs = 60;
+
+function stingsKillAnimal(beeStings, animalWeightLbs){
+    return stingsKillAnimal = beeStings * animalWeightLbs;
+}
+
+//“It takes X bee stings to kill this animal.
+console.log("It takes "+ stingsKillAnimal(beeStings, animalWeightLbs) + " bee stings to kill this animal.");
+*/
