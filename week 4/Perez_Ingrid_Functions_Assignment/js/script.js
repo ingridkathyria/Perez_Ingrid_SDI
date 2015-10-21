d66fb7b9db72501a07edc9d616d2214b6cf1f4f3
ingridkathyria <iperez@fullfail.edu>
2015-10-21 09:33:22 -0400

    added variable
    added variable for times clicked and the loop

--- week 4/Perez_Ingrid_Functions_Assignment/js/script.js
+++ week 4/Perez_Ingrid_Functions_Assignment/js/script.js
@@ -16,29 +16,32 @@ in	as	arguments	and	out	as	returned	values.
     e. All	user	prompts	must	be	validated	using	a	while	loop.
     */
 
-var pickLotto;//declaring variable for prompt
-var lotto;//declared variable
-var powerball;
-var timesclicked;
-timesclicked = 1;
-pickLotto = prompt("Would you like to play the Lotto or Powerball?");//asking if user would like to play the Lotto or Powerball
+
+
+
+var timesClicked = 1;//assigned value for times clicked
+var pickLotto = prompt("Would you like to play the Lotto or Powerball?");//asking if user would like to play the Lotto or Powerball
+//loop for timesClicked = ""
 while(pickLotto === ""){
-    prompt("Please enter a valid response. Would you like to play Lotto or Powerball");
-timesclicked++;
-    if(timesclicked === 3){
+    prompt("Please enter a valid response. Would you like to play Lotto or Powerball?");
+timesClicked++;
+    if(timesClicked === 3){
         console.log("I am not playing your games!");
         break;
     }
 }
 
 //Functions
-function lotteryNumbers(){
-
+function lotteryNumbers(lotto, powerball){
+var lotto = Math.random() *(53 - 1) + 1;
+    lotto = Math.round(lotto);
+var powerball = Math.random() *(59 - 1) +1;
+    powerball = Math.round(powerball);
 }
 
 
 if(pickLotto === ""){
     console.log("You are the weakest link, Goodbye!")
 }else {
-    console.log("Your numbers for " + +" are " + +".");
+    console.log("Your numbers for " + pickLotto  +" are " +  +".");
 }
