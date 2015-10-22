49088665763bb8ec31030fb7f42c2eff296a70ab
ingridkathyria <iperez@fullfail.edu>
2015-10-21 20:10:24 -0400

    added variables
    added variables, functions and outputs

--- week 4/Perez_Ingrid_Functions_Assignment/js/script.js
+++ week 4/Perez_Ingrid_Functions_Assignment/js/script.js
@@ -2,28 +2,17 @@
 //October 20,2015.
 //Functions Assignment
 
-
-
-/*
-//Brainstorm how to	find and return	a	group	of	random	numbers. You also need	to
-consider that your function	must return	either Powerball	or	regular	Florida	lottery
-numbers	but	not	both. Keep in mind that your final	project	must contain the following:
-   a. At least one function.
-b. Any functions	created must	use	arguments	and	parameters.
-    c. A	value	must	be	returned	for	each	function.
-d. You	must	not	use	“main	code”	variable	inside	of	a	function; you	must	pass	values
-in	as	arguments	and	out	as	returned	values.
-    e. All	user	prompts	must	be	validated	using	a	while	loop.
-    */
-
-
-
-
-var timesClicked = 1;//assigned value for times clicked
+//variables
 var pickLotto = prompt("Would you like to play the Lotto or Powerball?");//asking if user would like to play the Lotto or Powerball
-//loop for timesClicked = ""
-while(pickLotto === ""){
-    prompt("Please enter a valid response. Would you like to play Lotto or Powerball?");
+var randomLotto;//declared variables
+var randomPowerbNum;//declared variables
+var randomSingleBall;//declared variables
+var timesClicked = 1;//assigned value for times clicked
+
+
+//loop for timesClicked when user inputs = ""
+while(pickLotto === ""){//
+    prompt("Please enter a valid response. Would you like to play Lotto or Powerball?");//response when user inputs ""
 timesClicked++;
     if(timesClicked === 3){
         console.log("I am not playing your games!");
@@ -32,16 +21,48 @@ timesClicked++;
 }
 
 //Functions
-function lotteryNumbers(lotto, powerball){
-var lotto = Math.random() *(53 - 1) + 1;
-    lotto = Math.round(lotto);
-var powerball = Math.random() *(59 - 1) +1;
-    powerball = Math.round(powerball);
+function lottoNumbers(){
+    var lotto = [];
+for(i = 0; i < 6; i++) {
+     lotto[i] = Math.random() * (53 - 1) + 1;
+    lotto[i] = Math.round(lotto[i]);
+   
 }
+    return lotto;
+
+}
+
+function powerballNumbers(){
+    var powerball = [];
+    for(i=0; i< 5; i++) {
+        powerball[i] = Math.random() * (59 - 1) + 1;
+        powerball[i] = Math.round(powerball[i]);
+    }
+    return powerball;
+
+}
+function singlePowerball(){
+    var singlePowerb = Math.random() * (35 - 1) + 1;
+    singlePowerb = Math.round(singlePowerb);
+    return singlePowerb;
+
+}
+
+randomLotto = lottoNumbers(53, 1);
+randomPowerbNum = powerballNumbers(59, 1);
+randomSingleBall = singlePowerball(35, 1);
+
 
 
 if(pickLotto === ""){
     console.log("You are the weakest link, Goodbye!")
 }else {
-    console.log("Your numbers for " + pickLotto  +" are " +  +".");
+    if (pickLotto === "powerball") {
+        console.log(" Your powerball numbers are " + randomPowerbNum + " and your powerball is " + randomSingleBall + ".");
+    }else {
+        (pickLotto === "lotto")
+        {
+            console.log("Your lotto numbers are " + randomLotto + ".")
+        }
+    }
 }
